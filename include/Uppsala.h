c4c603ef2e81629df884380f6be3fd9ce0d21d46
Mhd Mahmoud Yehya <mahmoudyehya11@hotmail.com>
2023-10-26 18:29:21 +0200

    Added 2D Histogram

--- include/Uppsala.h
+++ include/Uppsala.h
@@ -48,22 +48,50 @@ void Data() {
         i += 1;
     }
 
-    TH1D* graph = new TH1D("graph", "Data; T; Data Count", 100, -20, 30);
+    //TH1D* graph = new TH1D("graph", "Data; T; Data Count", 50, -20, 30);
 
-    double x[Date.size()];
-    double y[Air_Temp.size()];
+    //double x[Date.size()];
+    //double y[Date.size()];
     for (int i=0; i < Date.size(); i++) {
-        x[i] = i;
-        y[i] = i;
-        graph->Fill(Air_Temp[i]);
+        //x[i] = i;
+        //y[i] = i;
+        //graph->Fill(Air_Temp[i]);
+    }
+    int size_date = Date.size();
+    //auto g = new TGraph(size_date, x, y);
+    TCanvas* c1 = new TCanvas("c1", "Plot", 900, 600);
+    //graph->SetMarkerStyle(20);
+    //graph->Draw();
+    //g->Draw();
+
+    //int j[Date.size()/1000];
+    //for (int i=0; i < Date.size()/1000; i++) {
+    //    j[i] = i;
+    //}
+
+    vector<double> Avg_Temp;
+    double Temp_Temp{0};
+    int prev_j{0};
+    for (int j=1; j < Date.size(); j++) {
+        Temp_Temp += Air_Temp[j];
+        if (Date[j][3] != Date[j-1][3]) {
+            Avg_Temp.push_back(Temp_Temp/(j-prev_j));
+            Temp_Temp = 0;
+            prev_j = j;
+        }
     }
 
-    //auto g = new TGraph(100, Air_Temp, Air_Temp);
-    TCanvas* c1 = new TCanvas("c1", "Plot", 900, 600);
-    graph->SetMarkerStyle(20);
-    graph->Draw();
+    TH2D* ggraph = new TH2D("ggraph", "Data; Date; Temp", Avg_Temp.size(), 0, Avg_Temp.size(), 50, 2, 8);
 
-    //for (unsigned int i=0; i < Date.size(); i++)
+    for (int i=0; i < Avg_Temp.size(); i++) {
+        ggraph->Fill(i, Avg_Temp[i]);
+    }
+
+    ggraph->SetMarkerStyle(20);
+    ggraph->Draw();
+
+    for (unsigned int i=0; i < Avg_Temp.size(); i++)
+        cout << Avg_Temp[i] << endl;
     //    cout << Air_Temp[i] << endl;
         //cout << Date[i] << endl;
     //    cout << Quality[i] << endl;
